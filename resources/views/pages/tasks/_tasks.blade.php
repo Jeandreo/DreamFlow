088b693a9eae60e37a0b4ae45b7bcd009944c3f8
JeandreoFurquim <jeandreofur@gmail.com>
2024-07-11 16:20:51 -0300

    Update

--- resources/views/pages/tasks/_tasks.blade.php
+++ resources/views/pages/tasks/_tasks.blade.php
@@ -7,7 +7,7 @@
 	</div>
 	<!-- BEGIN:SUB-TASK -->
 	<div class="subtasks-zone subtasks-zone-{{ $task->id }}" @if($task->open_subtasks == false) style="display: none;" @endif id="subtasks-zone-{{ $task->id }}">
-		@foreach ($task->subtasks()->where('status', 1)->orderBy('order', 'ASC')->orderBy('date', 'ASC')->get() as $subtask)
+		@foreach ($task->subtasks()->where('status', 1)->orderBy('order', 'ASC')->get() as $subtask)
 		@include('pages.tasks._subtask')
 		@endforeach
 	</div>
