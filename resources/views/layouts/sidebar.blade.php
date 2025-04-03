bdf4469f61ab7de6d9360c49aef0cb1b08337f44
JeandreoFurquim <jeandreofur@gmail.com>
2025-04-02 23:16:07 -0300

    Update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -137,7 +137,7 @@
                                 </span>
                                 <span class="menu-badge">
                                     <span class="badge badge-custom text-gray-700">
-                                        {{ $project->tasks()->where('status', true)->where('checked', false)->count() }}
+                                        {{ $project->tasks()->where('status', true)->where('designated_id', Auth::id())->where('checked', false)->count() }}
                                     </span>
                                 </span>
                             </a>
